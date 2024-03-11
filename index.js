d42b1d0a739e0b5c315accd66e0dcc4f90a047d4
amitabhdas96 <amitabh.sweden@gmail.com>
2024-03-11 18:39:42 +1300

    1

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log(1/0) // division by zero = Infinity
+console.log(-1/0) // negative division by zero = -Infinity
+console.log("not a number" / 1) // invalid mathematical operation = NaN
+console.log(typeof(5));
+console.log(typeof(5.5));
+console.log(typeof("5"));
